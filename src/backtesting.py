f2833f12b4ecc58da9d75d1e9bc1e57d2a18dd3d
Aanjneya Moudgil <a.moudgil@tum.de>
2026-02-25 03:20:40 +0100

    added dummy_case

--- src/backtesting.py
+++ src/backtesting.py
@@ -1,15 +1,18 @@
 import polars as pl
-from src.matching_engine import Matching_Engine
-from src.portfolio import Portfolio
+from matching_engine import Matching_Engine
+from portfolio import Portfolio
 import glob
+from strategy import Strategy
 
 
 class Backtesting:
     def __init__(self):
         self.match = Matching_Engine()
         self.portfolio = Portfolio(10000)
-        self.data = glob.glob("data/raw/*.parquet")
+        self.data = glob.glob("src/data/raw/*.parquet")
+        print("Found", len(self.data), "files!")
         self.data.sort()
+        self.strategy = Strategy()
 
     def run(self):
         for filename in self.data:
@@ -17,9 +20,14 @@ class Backtesting:
             asks = df["ask_0_p"].to_list()
             bids = df["bid_0_p"].to_list()
             times = df["event_time"].to_list()
+
             for cur_ask, cur_bid, cur_time in zip(asks, bids, times):
                 trades = self.match.execute_order(cur_ask, cur_bid, cur_time)
+                ls = self.strategy.on_tick(cur_ask, cur_bid, cur_time)
+                self.match.add_orders(ls)
+
                 for j in trades:
                     self.portfolio.get_trade(j)
+                    self.strategy.on_execution()
 
                 self.portfolio.update((cur_ask + cur_bid)/2)
