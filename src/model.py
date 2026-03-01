d319146f00f46c8b6b1350089c7d38cf958ada28
Aanjneya Moudgil <a.moudgil@tum.de>
2026-03-01 16:50:27 +0100

    added lstm

--- src/model.py
+++ src/model.py
@@ -2,13 +2,20 @@ from torch import nn
 
 class Model(nn.Module):
     def __init__(self):
-        super().__init__()
+        super(Model, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, (1,2))
         self.conv2 = nn.Conv2d(32, 32, (1,2))
-        self.m = nn.LeakyReLU()
+        self.relu = nn.LeakyReLU()
+        self.lstm = nn.LSTM(32*138,64,batch_first=True)
+        self.linear = nn.Linear(64,3)
 
     def forward(self, x):
         x = x.unsqueeze(1)
-        temp = self.m(self.conv1(x))
-        temp = self.m(self.conv2(temp))
-        return temp
+        ts = self.relu(self.conv1(x))
+        ts = self.relu(self.conv2(ts))
+        ts = ts.permute([0,2,1,3])
+        ts = ts.reshape(ts.shape[0], ts.shape[1], -1)
+        lstm_out, _ = self.lstm(ts)
+        last_out = lstm_out[:,-1,:]
+        state = self.linear(last_out)
+        return state
