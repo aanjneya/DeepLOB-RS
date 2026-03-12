3f3f3d3abf176a59a0c324270a29843a4bae2c05
Aanjneya Moudgil <a.moudgil@tum.de>
2026-03-12 17:24:00 +0100

    updated dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -3,12 +3,16 @@ from torch.utils.data import DataLoader, Dataset
 
 class LOBDataset(Dataset):
     def __init__(self, df, window_size):
-        self.df = df
         self.window_size = window_size
-        self.data = torch.tensor(self.df.to_numpy()).float()
+
+        x_df = df.drop("label")
+        self.x_data = torch.tensor(x_df.to_numpy()).float()
+
+        y_df = df.select("label")
+        self.y_data = torch.tensor(y_df.to_numpy()).long().squeeze()
 
     def __len__(self):
-        return self.df.height - self.window_size
+        return len(self.x_data) - self.window_size
 
     def __getitem__(self, idx):
-        return self.data[idx:idx+self.window_size]
+        return self.x_data[idx:idx+self.window_size], self.y_data[idx+self.window_size-1]
